ba9fcadc9cc8650cd76b4e0b37adfd3fe2dda4ac
Saini Vijay <vijaysainiprofessional@gmail.com>
2022-09-08 08:24:07 +0530

    docker file added

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Docker World!");
+app.MapGet("/", () => "Hello Docker World new app!");
 
 app.Run();
