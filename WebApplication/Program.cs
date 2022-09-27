c919d14e654fa59c82c9c90e55188e85ef32f58f
Saini Vijay <vijaysainiprofessional@gmail.com>
2022-09-27 09:12:57 +0530

    cece

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Github vij!");
+app.MapGet("/", () => "Hello Github vij123123!");
 
 app.Run();
