2fb19816b59acb17bfc33c3dd5a5e0ce270441c4
Saini Vijay <vijaysainiprofessional@gmail.com>
2022-09-28 09:11:02 +0530

    edwfew

--- WebApplication02/Program.cs
+++ WebApplication02/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!33333333333");
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
